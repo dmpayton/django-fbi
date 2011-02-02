52c95e16922f28382ae3bed270f6478bbf279199
dmpayton <derek@cukerinteractive.com>
2011-02-02 11:04:15 -0800

    set an unusable password when the user is created

--- django_fbi/backends/simple.py
+++ django_fbi/backends/simple.py
@@ -7,10 +7,12 @@ class SimpleBackend(DefaultBackend):
             created with the username "FB_<facebook_id>", logged in, and
             redirected. Requires 'email' permission from Facebook.
         '''
-        self.user = User.objects.create(
+        self.user = User(
             username='FB_%s' % self.facebook.profile['id'],
             email=self.facebook.profile['email']
             )
+        self.user.set_unusable_password()
+        self.user.save()
         profile = self._get_user_profile()
         profile.facebook_id = self.facebook.profile['id']
         profile.facebook_email = self.facebook.profile['email']
