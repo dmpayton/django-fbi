e0b2372eed36ca725256461ddcaefb6f24f817a2
dmpayton <derek.payton@gmail.com>
2011-09-07 09:58:22 -0700

    cache the connect app in _thread_locals during the request/response cycle

--- django_fbi/managers.py
+++ django_fbi/managers.py
@@ -1,11 +1,18 @@
 from django.core.exceptions import ImproperlyConfigured
 from django.db import models
+from django_fbi.middleware import _thread_locals
 
 class FacebookAppManager(models.Manager):
     def connect(self):
         try:
-            return self.model.objects.get(connect=True)
-        except (self.model.DoesNotExist):
-            raise ImproperlyConfigured('No Facebook app is setup for Connect.')
-        except (self.model.MultipleObjectsReturned):
-            raise ImproperlyConfigured('Multiple Facebook apps are setup for Connect.')
+            ## If we're in a request/response cycle and the middleware
+            ## is installed, we should already have the app.
+            return _thread_locals.connect
+        except AttributeError:
+            ## It's not cached in _thread_locals, look it up.
+            try:
+                return self.model.objects.get(connect=True)
+            except (self.model.DoesNotExist):
+                raise ImproperlyConfigured('No Facebook app is setup for Connect.')
+            except (self.model.MultipleObjectsReturned):
+                raise ImproperlyConfigured('Multiple Facebook apps are setup for Connect.')
---
+++ django_fbi/middleware.py
@@ -0,0 +1,27 @@
+from django.core.exceptions import ImproperlyConfigured
+from django_fbi.models import FacebookApp
+try:
+    from threading import local
+except ImportError:
+    from django.utils._threading_local import local
+
+_thread_locals = local()
+
+class FacebookConnectMiddleware(object):
+    def process_request(self, request):
+        _thread_locals.request = request
+        try:
+            _thread_locals.connect = FacebookApp.objects.connect()
+        except ImproperlyConfigured:
+            pass
+
+    def process_response(self, request, response):
+        try:
+            del _thread_locals.request
+        except AttributeError:
+            pass
+        try:
+            del _thread_locals.connect
+        except AttributeError:
+            pass
+        return response
