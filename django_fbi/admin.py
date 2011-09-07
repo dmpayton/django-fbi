215ad4facdb7d3f50d2ad9f26a5164731f66f24b
dmpayton <derek.payton@gmail.com>
2011-09-07 09:55:14 -0700

    validate tab/canvas templates

--- django_fbi/admin.py
+++ django_fbi/admin.py
@@ -1,23 +1,27 @@
 from django import forms
 from django.contrib import admin
+from django.template import loader
+from django.template.base import TemplateDoesNotExist
 from django_fbi.models import FacebookApp, FacebookAccount
 
 class FacebookAppForm(forms.ModelForm):
 
-    def _validate_template(self, template_path):
-        ## TODO: validate that template_path is a valid template
-        return True
+    def _validate_template(self, template):
+        if template:
+            try:
+                loader.find_template(template)
+            except TemplateDoesNotExist:
+                raise forms.ValidationError('Template does not exist.')
+            return True
 
     def clean_canvas_template(self):
         template = self.cleaned_data.get('canvas_template')
-        if template and not self._validate_template(template):
-            raise forms.ValidationError('Template does not exist.')
+        self._validate_template(template)
         return template
 
     def clean_tab_template(self):
         template = self.cleaned_data.get('tab_template')
-        if template and not self._validate_template(template):
-            raise forms.ValidationError('Template does not exist.')
+        self._validate_template(template)
         return template
 
     class Meta:
