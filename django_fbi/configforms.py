facc4571e629b86bdd7755debca08203700eb08e
Derek Payton <derek.payton@gmail.com>
2011-09-15 08:55:26 +0000

    let the scope be optional

--- django_fbi/configforms.py
+++ django_fbi/configforms.py
@@ -5,7 +5,7 @@ from django import forms
 class FacebookAuthInfoForm(ConfigurationForm):
     app_id = forms.CharField(max_length=50)
     app_secret = forms.CharField(max_length=255)
-    scope = forms.CharField(max_length=255, help_text='')
+    scope = forms.CharField(max_length=255, required=False)
 
 instance = ConfigurationInstance('fb-auth', 'Facebook Auth Credentials', FacebookAuthInfoForm)
 register(instance)
